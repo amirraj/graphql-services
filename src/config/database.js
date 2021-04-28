a5e3b71cf08ce986d0daa227c28c0ce2f8901368
amirraj <amirraj20@gmail.com>
2021-04-28 15:31:03 +0600

    factory design skeleton

--- src/config/database.js
+++ src/config/database.js
@@ -2,11 +2,6 @@ const { Sequelize } = require('sequelize');
 const dotenv = require('dotenv');
 dotenv.config();
 
-// module.exports = new Sequelize(process.env.DATABASE_URL, {
-//   host: 'localhost',
-//   dialect: 'postgres'
-// });
-
 exports.ppi_db = new Sequelize(process.env.DATABASE_URL, {
   host: 'localhost',
   dialect: 'postgres'
