bc7e8961b2823a0df8c0ee39fca5b33980e51a18
sifatnabil <sifatnabil@gmail.com>
2021-10-12 13:01:01 +0600

    Changed Query Parameters for HCP

--- src/server/gds.resolver-test.js
+++ src/server/gds.resolver-test.js
@@ -4,9 +4,45 @@ const conferencesModel = require("./gds-conferences.model");
 const guidelinesModel = require("./gds-guidelines.model");
 const clinicalTrialsModel = require("./gds-clinical-trials.model");
 
+const filterKeys = (dict) => {
+  Object.keys(dict.where).forEach((key, index) => {
+    if (dict.where[key] === undefined) {
+      delete dict.where[key];
+    }
+  });
+
+  return dict;
+};
+
 const Query = {
-  getHCPs: () => {
-    return hcpModel.findAll();
+  getHCPs: (root, args) => {
+    let query = {};
+    let where = {
+      where: {
+        expert_id: args.expert_id,
+        onekey_id: args.onekey_id,
+        country: args.country,
+        full_name: args.full_name,
+        first_name: args.first_name,
+        middle_name: args.middle_name,
+        last_name: args.last_name,
+        affiliation: args.affiliation,
+      },
+    };
+
+    dict = filterKeys(where);
+
+    query =
+      args.offset || args.limit
+        ? {
+            ...args,
+            ...dict,
+          }
+        : dict;
+
+    const hcp_details = hcpModel.findAll(query);
+
+    return hcp_details;
   },
   getPublications: () => publicationsModel.findAll(),
   getGuidelines: () => guidelinesModel.findAll(),
