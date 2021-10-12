78038a4494031d2e95b02c3a7aaa2ed29cedd570
sifatnabil <sifatnabil@gmail.com>
2021-10-12 18:24:15 +0600

    Tested with axios

--- test.js
+++ test.js
@@ -1,7 +1,33 @@
-const excelToJson = require("convert-excel-to-json");
+const axios = require("axios");
 
-const result = excelToJson({
-  sourceFile: "gds_data.xlsx",
-});
-
-console.log(result["Basic"]);
+axios({
+  url: "http://localhost:5000/gds",
+  method: "post",
+  data: {
+    query: `
+    {
+      getHCPs(first_name: "Silvio"){
+        expert_id
+        full_name
+        first_name
+        publication {
+          id
+        }
+        conference {
+          id
+        }
+        guideline {
+          id
+        }
+        clinicalTrial {
+          id
+        }
+      }
+    }
+      `,
+  },
+})
+  .then((response) => {
+    console.log(JSON.stringify(response.data.data));
+  })
+  .catch((err) => console.log(err));
